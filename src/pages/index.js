5056fd96639137cd29d6a38b6558569e8892c650
vishu <bansalvishu56@gmail.com>
2023-04-02 00:36:10 +0530

    made first section successfully

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import {BsFillMoonStarsFill} from "react-icons/bs"
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram} from "react-icons/ai"
+import Image from "next/image"
+import deved from "../../public/dev-ed-wave.png"
 
 export default function Home() {
   return (
@@ -9,9 +13,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className=" bg-white px-10">
         <section className=" min-h-screen">
-          <nav></nav>
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Vishu Bansal</h1>
+            <ul className="flex items-center">
+              <li><BsFillMoonStarsFill className=" cursor-pointer text-2xl"/></li>
+              <li><a href="#" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">Resume</a></li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Vishu Bansal</h2>
+            <h3 className=" text-2xl py-2">Full Stack Web Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              A Engineering Student who is interested in programming and various other fields. Just exploring different things.
+              I am also a freelancer that can fulfill your development needs quickly and elegantly.
+            </p>
+          </div>
+          <div className=" text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle/>
+            <AiFillLinkedin/>
+            <AiFillInstagram/>
+          </div>
+          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover" alt="" />
+          </div>
         </section>
       </main>
     </div>
